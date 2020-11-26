8a61120c695310b64b5f44ace6889ce2ad8a09b4
Alan <alan.h@hcp.tech>
2020-11-27 00:39:45 +0800

    添加@Override注解

--- src/com/kbb/servlet/MainServlet.java
+++ src/com/kbb/servlet/MainServlet.java
@@ -1,7 +1,7 @@
 package com.kbb.servlet;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
+import javax .servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -10,10 +10,12 @@ import java.io.PrintWriter;
 public class MainServlet extends HttpServlet {
     private String message;
 
+    @Override
     public void init() throws ServletException {
         message = "Hello world, this message is from servlet! Alan...";
     }
 
+    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //设置响应内容类型
         resp.setContentType("text/html");
@@ -23,6 +25,7 @@ public class MainServlet extends HttpServlet {
         out.println("<h1>" + message + "</h1>");
     }
 
+    @Override
     public void destroy() {
         super.destroy();
     }
