50888500dccd7f02f5c6a34f6fd2bd6d08076a64
Alan <alan.he@fowo.com>
2020-11-26 15:58:37 +0800

    初始化工程

---
+++ src/MainServlet.java
@@ -0,0 +1,27 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MainServlet extends HttpServlet {
+    private String message;
+
+    public void init() throws ServletException {
+        message = "Hello world, this message is from servlet! Alan...";
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //设置响应内容类型
+        resp.setContentType("text/html");
+
+        //设置逻辑实现
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>" + message + "</h1>");
+    }
+
+    public void destroy() {
+        super.destroy();
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: gd
+  Date: 2020/11/26
+  Time: 12:36 下午
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  Hello World!
+  您好！欢迎光临...
+  </body>
+</html>
